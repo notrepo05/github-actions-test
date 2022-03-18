716fa43b476abd975fbfb9ca69590e20a7aafa92
Nick Rohn <nrohn@vmware.com>
2022-03-18 08:57:32 -0700

    programmatic detect

--- .github/tasks/detect.sh
+++ .github/tasks/detect.sh
@@ -4,12 +4,31 @@ set -eux
 
 function main() {
   echo "${STEP}"
+
+  local scenarios
+  scenarios='[]'
+  local supported_scenarios
+  supported_scenarios=""
+  if [[ "${PUBLISH_TAS}" == true ]]; then
+    supported_scenarios+="tas"
+  fi
+  if [[ "${PUBLISH_IST}" == true ]]; then
+    supported_scenarios+=" ist"
+  fi
+  if [[ "${PUBLISH_TASW}" == true ]]; then
+    supported_scenarios+=" tasw"
+  fi
+
+  for scenario in $supported_scenarios; do
+    senarios=$(jq --arg scenario "${scenario}" '. += [$scenario]' <<< "${scenarios}")
+  done
+
   echo "tas: ${PUBLISH_TAS}"
   echo "ist: ${PUBLISH_IST}"
   echo "tasw: ${PUBLISH_TASW}"
   echo "hello world"
-  echo "::set-output name=products::[\"tas\", \"ist\"]"
-  echo "::set-output name=step::publish"
+  echo "::set-output name=products::$(jq -c "." <<< "${scenarios}")"
+  echo "::set-output name=step::${STEP}"
 }
 
 main "$@"
